5c62222827549ca92ef024dc686d31abfd8de66c
JIT KWONG CHEO <jitkcheo@JITs-MacBook-Pro.local>
2023-09-20 05:31:03 +1000

    inductions controllers and abit readme.md done

--- controllers/inductions_controllers copy.py
+++ controllers/inductions_controllers copy.py
@@ -8,68 +8,68 @@ from schemas.inductions import induction_schema, inductions_schema
 # /induction
 inductions = Blueprint("induction", __name__, url_prefix="/inductions")
 
-@equipments.errorhandler(ValidationError)
+@inductions.errorhandler(ValidationError)
 def key_error_handler(e):
     return jsonify({"error": f"Validation Error - `{e}`"}), 400
 
-# /equipments -> list of equipments
-@equipments.route("/", methods=["GET"])
-def get_equipments():
-    q = db.select(Equipment)
-    equipments = db.session.scalars(q)
-    return jsonify(equipments_schema.dump(equipments))
+# /inductions -> list of inductions
+@inductions.route("/", methods=["GET"])
+def get_inductions():
+    q = db.select(Induction)
+    inductions = db.session.scalars(q)
+    return jsonify(inductions_schema.dump(inductions))
     
-# /equipments/<id> -> show equipment with id
-@equipments.route("/<int:equipment_id>", methods=["GET"])
-def get_equipment(equipment_id: int):
-    q = db.select(Equipment).filter_by(id=equipment_id)
-    equipment = db.session.scalar(q)
-    response = equipment_schema.dump(equipment)
+# /inductions/<id> -> show induction with id
+@inductions.route("/<int:induction_id>", methods=["GET"])
+def get_induction(induction_id: int):
+    q = db.select(Induction).filter_by(id=induction_id)
+    induction = db.session.scalar(q)
+    response = induction_schema.dump(induction)
 
     if response:
         return jsonify(response)
 
-    return jsonify(message=f"Equipment with id=`{equipment_id}` not found")
+    return jsonify(message=f"Induction with id=`{equipment_id}` not found")
 
     
-# /equipments/<id> -> delete equipment with id
-@equipments.route("/<int:equipment_id>", methods=["DELETE"])
-def delete_equipment(equipment_id: int):
-    q = db.select(Equipment).filter_by(id=equipment_id)
-    equipment = db.session.scalar(q)
-    response = equipment_schema.dump(equipment)
+# /inductions/<id> -> delete induction with id
+@inductions.route("/<int:induction_id>", methods=["DELETE"])
+def delete_induction(induction_id: int):
+    q = db.select(Induction).filter_by(id=induction_id)
+    induction = db.session.scalar(q)
+    response = induction_schema.dump(induction)
 
     if response:
-        db.session.delete(equipment)
+        db.session.delete(induction)
         db.session.commit()
-        return jsonify(message=f"Equipment with id=`{equipment_id}` deleted successfully!")
+        return jsonify(message=f"Induction with id=`{induction_id}` deleted successfully!")
 
-    return jsonify(message=f"Cannot delete equipment with id=`{equipment_id}`. Not found")
+    return jsonify(message=f"Cannot delete induction with id=`{induction_id}`. Not found")
 
-# /equipments -> Creating a equipment
-@equipments.route("/", methods=["POST"])
-def create_equipments():
-    equipment_json = equipment_schema.load(request.json)
-    equipment = Equipment(**equipment_json)
-    db.session.add(equipment)
+# /inductions -> Creating a induction
+@inductions.route("/", methods=["POST"])
+def create_inductions():
+    induction_json = induction_schema.load(request.json)
+    induction = Induction(**induction_json)
+    db.session.add(induction)
     db.session.commit()
 
-    return jsonify(equipment_schema.dump(equipment))
+    return jsonify(induction_schema.dump(induction))
 
-# /equipments/<id> -> Updating a equipment with id
-@equipments.route("/<int:equipment_id>", methods=["PUT"])
-def update_equipments(equipment_id: int):
-    q = db.select(Equipment).filter_by(id=equipment_id)
-    equipment = db.session.scalar(q)
-    response = equipment_schema.dump(equipment)
+# /inductions/<id> -> Updating a induction with id
+@inductions.route("/<int:induction_id>", methods=["PUT"])
+def update_inductions(induction_id: int):
+    q = db.select(Induction).filter_by(id=induction_id)
+    induction = db.session.scalar(q)
+    response = induction_schema.dump(induction)
 
     if response:
-        equipment_json = equipment_schema.load(request.json)
-        equipment.equipment_number = equipment_json["equipment_number"]
-        equipment.type_equipment = equipment_json["type_equipment"]
-        equipment.description = equipment_json["description"]
-        equipment.user_id = equipment_json["user_id"]
+        induction_json = induction_schema.load(request.json)
+        induction.expiry_date = induction_json["expiry_date"]
+        induction.documents_required = induction_json["documents_required"]
+        induction.description = induction_json["description"]
+        induction.location = induction_json["location"]
         db.session.commit()
-        return jsonify(equipment_schema.dump(equipment))
+        return jsonify(induction_schema.dump(induction))
 
-    return jsonify(message=f"Cannot update equipment with id=`{equipment_id}`. Not found")
+    return jsonify(message=f"Cannot update induction with id=`{induction_id}`. Not found")
